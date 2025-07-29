7db381117114968be5dd7e3f4a55aba8c5767ca1
Khayitbek03 <xayitbekmirsoatov@gmail.com>
2025-07-29 15:55:01 +0500

    refactor: add changes in SearchBar test cases based on feedback

--- src/components/__tests__/SearchBar.test.tsx
+++ src/components/__tests__/SearchBar.test.tsx
@@ -1,50 +1,38 @@
-import { describe, it, expect, vi } from 'vitest';
-import { render, screen, fireEvent } from '@testing-library/react';
+/// <reference types="vitest/globals" />
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SearchBar from '../SearchBar';
+import { vi } from 'vitest';
 
 describe('SearchBar Component', () => {
   it('renders input and button', () => {
-    render(
-      <SearchBar value="" onChange={() => {}} onSearch={() => {}}></SearchBar>,
-    );
+    render(<SearchBar value="" onChange={vi.fn()} onSearch={vi.fn()} />);
     expect(screen.getByPlaceholderText(/search by name/i)).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /search/i })).toBeInTheDocument();
   });
+
   it('displays the correct input value', () => {
-    render(
-      <SearchBar
-        value="pikachu"
-        onChange={() => {}}
-        onSearch={() => {}}
-      ></SearchBar>,
-    );
+    render(<SearchBar value="pikachu" onChange={vi.fn()} onSearch={vi.fn()} />);
     expect(screen.getByDisplayValue('pikachu')).toBeInTheDocument();
   });
-  it('calls onChange when typing in input', () => {
-    const handleChange = vi.fn();
-    render(
-      <SearchBar
-        value=""
-        onChange={handleChange}
-        onSearch={() => {}}
-      ></SearchBar>,
-    );
-    fireEvent.change(screen.getByPlaceholderText(/search by name/i), {
-      target: { value: 'bulbasaur' },
-    });
-    expect(handleChange).toHaveBeenCalledTimes(1);
-  });
-  it('calls onSearch when clicking the search button', () => {
-    const handleSearch = vi.fn();
-    render(
-      <SearchBar
-        value=""
-        onChange={() => {}}
-        onSearch={handleSearch}
-      ></SearchBar>,
-    );
 
-    fireEvent.click(screen.getByRole('button', { name: /search/i }));
+  it('calls onChange when typing in input', async () => {
+    const handleChange = vi.fn();
+    render(<SearchBar value="" onChange={handleChange} onSearch={vi.fn()} />);
+    const input = screen.getByPlaceholderText(/search by name/i);
+
+    await userEvent.type(input, 'bulbasaur');
+
+    expect(handleChange).toHaveBeenCalled();
+    expect(handleChange).toHaveBeenCalledTimes('bulbasaur'.length);
+  });
+
+  it('calls onSearch when clicking the search button', async () => {
+    const handleSearch = vi.fn();
+    render(<SearchBar value="" onChange={vi.fn()} onSearch={handleSearch} />);
+    const button = screen.getByRole('button', { name: /search/i });
+
+    await userEvent.click(button);
 
     expect(handleSearch).toHaveBeenCalledTimes(1);
   });
